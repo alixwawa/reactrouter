05cae3d10e1c40b54b38069d4ffa1148e8377b6b
Alix Wawa <alixwawa@yahoo.com>
2023-06-19 22:13:15 -0500

    using useNavigation to give us hints of navigation status

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
+	// const navigation = useNavigation();
+
 	return (
 		<>
-            <MainNavigation />
-            <main>
-                <Outlet />
-            </main>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
 		</>
 	);
 };
